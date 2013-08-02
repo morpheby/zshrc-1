ee4c3465db5321902f2830a75ae0d06fd3dc102e
Ilya Mikhaltsou <morpheby@gmail.com>
2013-08-02 17:22:11 +0300

    Fixed once again
    Signed-off-by: Ilya Mikhaltsou <morpheby@gmail.com>

--- prompt.zsh
+++ prompt.zsh
@@ -102,12 +102,12 @@ function promptSetup () {
     
     # Finally, the prompt.
     PS1=$'\n'                     # newline (specially quotet, see zsh FAQ 3.13)
-    PS1+=%(?..$'%K{12}%F{46}%Breturned %?\n')    # output last error number if present
-    PS1+=$PWD_PART$'\n'
+    PS1+=$NOCOLOR%(?..$'%K{12}%F{46}%Breturned %?%k\n')    # output last error number if present
+    PS1+=$PWD_PART$NOCOLOR$'\n'
+    PS1+="%{$terminfo_down_sc$VCS_LINE$terminfo[rc]%}" # the second line
     PS1+=$PR_STITLE               # tmux title if present
     PS1+=$PR_VCSSIGN              # version control part if present
     PS1+=$PR_SIGN                 # the user sign
-    PS1+="%{$terminfo_down_sc$VCS_LINE$terminfo[rc]%}" # the second line
     PS1+=" "                      # an additional space
 
     # reset the tmux title
